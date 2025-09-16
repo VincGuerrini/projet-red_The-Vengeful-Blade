a90cf9a74a8d842b9c859633fd864cbd6fb81967
Yokasashi <vincentguerrini07@gmail.com>
2025-09-16 11:07:15 +0200

    Ajout entiter de la montagne

--- src/internal/models/monster.go
+++ src/internal/models/monster.go
@@ -18,7 +18,7 @@ func InitSanglier() Monster {
 	return Monster{
 		Name:         "Sanglier",
 		HPmax:        15,
-		Attack:       rand.Intn(4) + 5,
+		Attack:       rand.Intn(4) + 5, // Me permet a mon entiter de faire entre 5 et 9 dommage
 		Initiative:   3,
 		PatternState: 0,
 	}
@@ -48,6 +48,36 @@ func InitTavernier() Monster {
 	return Monster{
 		Name:         "Tavernier",
 		HPmax:        40,
+		Attack:       rand.Intn(6) + 7,
+		Initiative:   4,
+		PatternState: 0,
+	}
+}
+
+func InitOurs() Monster {
+	return Monster{
+		Name:         "Ours",
+		HPmax:        45,
+		Attack:       rand.Intn(6) + 8,
+		Initiative:   5,
+		PatternState: 0,
+	}
+}
+
+func InitEsprit() Monster {
+	return Monster{
+		Name:         "Esprit",
+		HPmax:        25,
+		Attack:       rand.Intn(4) + 5,
+		Initiative:   4,
+		PatternState: 0,
+	}
+}
+
+func InitBucheron() Monster {
+	return Monster{
+		Name:         "Bûcheron",
+		HPmax:        42,
 		Attack:       rand.Intn(6) + 7,
 		Initiative:   4,
 		PatternState: 0,
